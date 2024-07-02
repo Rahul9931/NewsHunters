b4b2a8392833bd234b504a8d1c7529c460134def
Rahul9931 <rahulsaini@gmail.com>
2024-07-02 09:49:29 +0530

    add files

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView, StyleSheet, Text, View} from 'react-native'
+import Home from './App/Screen/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>rahul saini</Text>
+      {/*<Home/>*/}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container:{
+    flex:1,
+    backgroundColor:'white',
+    paddingTop:20,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
